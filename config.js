5ee94440e26157551334378827c9f28b65959543
DARK-X-MD <Darkxmd2024@gmail.com>
2024-10-12 13:40:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "neRVlS6S#-J6199czDgalt6huvtzJK-kao2u3ye3_9I_htA0TVfw",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
